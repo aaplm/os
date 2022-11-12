6cd3620864fc8674e6122f715b1add7cd0b3af49
Ambrose Mercado <83136113+aaplm@users.noreply.github.com>
2022-11-11 18:47:01 -0700

    added target triple for compiling

---
+++ README.md
@@ -0,0 +1,4 @@
+# OS
+
+This repo is the result of following along
+with [Philipp Oppermann's](https://github.com/phil-opp) [Writing an OS in Rust](https://os.phil-opp.com/) tutorial.
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@
 
 use core::panic::PanicInfo;
 
+/// Don't mangle the entry point, as we want this to remain `_start`.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	#[allow(clippy::empty_loop)]
-	loop {}
+    #[allow(clippy::empty_loop)]
+    loop {}
 }
 
+/// This is our handler for any panics that occur.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-	loop {}
+    #[allow(clippy::empty_loop)]
+    loop {}
 }
