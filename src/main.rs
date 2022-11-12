a88787d47f23a1ffacd57c9319dbf4871e853f12
Ambrose Mercado <83136113+aaplm@users.noreply.github.com>
2022-11-11 19:14:18 -0700

    added small VGA hello world

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #![no_std]
 #![no_main]
 
+use core::intrinsics::offset;
 use core::panic::PanicInfo;
 
 /// Don't mangle the entry point, as we want this to remain `_start`.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     #[allow(clippy::empty_loop)]
     loop {}
 }
